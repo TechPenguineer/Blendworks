33b11bb0f78e3bec5522bb3caa3bb7ce71d623ab
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-04 18:09:59 -0400

    create rigid body type

--- physics/bwu_physics_op.py
+++ physics/bwu_physics_op.py
@@ -3,13 +3,23 @@ import bpy
 class MyClassName(bpy.types.Operator):
     bl_idname = "object.apply_rigidbody"
     bl_label = "Apply Rigidbody"
-    bl_description = "Description that shows in blender tooltips"
-    bl_options = {"REGISTER"}
+    bl_description = "Adds a rigidbody to the selected object(s)"
 
     @classmethod
-    def poll(cls, context):
-        return True
-
-    def execute(self, context):
-        
-        return {"FINISHED"}
+    def poll(cls,context):
+        obj = context.object
+        if obj is not None:
+            if obj.mode == "OBJECT":
+                return True
+        return False
+            
+    def execute(self,context):
+            active_obj = context.view_layer.objects.active 
+            
+            obNameList=[]
+            for objs in active_obj:
+                obNameList.append(objs.name) 
+                bpy.context.rigid_body.type = 'ACTIVE'
+                bpy.context.space_Data
+            
+            return { 'FINISHED' }
