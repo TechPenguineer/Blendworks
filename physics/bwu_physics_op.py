ed8ac4f7aff563ad9c8ad6298c36c02d996935e5
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-04 18:25:58 -0400

    made passive type

--- physics/bwu_physics_op.py
+++ physics/bwu_physics_op.py
@@ -23,3 +23,27 @@ class  BWU_OT_Add_Rigidbody_Op(bpy.types.Operator):
                 bpy.ops.rigidbody.object_add(type='ACTIVE')
             
             return { 'FINISHED' }
+
+class  BWU_OT_Add_Rigidbody_Passive_Op(bpy.types.Operator):
+    bl_idname = "object.apply_passive_rigidbody"
+    bl_label = "Make Passive"
+    bl_description = "Adds a passive rigidbody to the selected object(s)"
+
+    @classmethod
+    def poll(cls,context):
+        obj = context.object
+        if obj is not None:
+            if obj.mode == "OBJECT":
+                return True
+        return False
+            
+    def execute(self,context):
+            selected_objects = bpy.context.selected_objects
+            
+            obNameList=[]
+            context = bpy.context
+            scene = context.scene
+            for objs in selected_objects:
+                bpy.ops.rigidbody.object_add(type='PASSIVE')
+            
+            return { 'FINISHED' }
