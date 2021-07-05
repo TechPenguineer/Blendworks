a59a37f25855f354db9d257a500f11ff846c48b5
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-04 23:17:30 -0400

    Update make_item_poly.py

--- polify/make_item_poly.py
+++ polify/make_item_poly.py
@@ -11,8 +11,5 @@ class change_lod(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        for area in bpy.data.screen["Blendworks Polify"].screens[0].areas: 
-            if area.type == 'VIEW_3d':
-                for spaces in area.spaces:
-                    area.spaces.active.clip_start = 0.1
+        bpy.context.window.workspace=bpy.data.workspace[self.layoutItems1]
         return {"FINISHED"}
