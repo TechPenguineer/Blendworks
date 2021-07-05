38b938edfeb85e4b17f4262cd84fe35c10509bb7
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-04 23:46:16 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,9 +19,10 @@ from . bwu_pnl import BWU_PHYSICS_PANEL, POLY_WINDOW_CONTEXT
 from . physics.bwu_physics_op import BWU_OT_Add_Rigidbody_Op, BWU_OT_Add_Rigidbody_Passive_Op, BWU_OT_Remove_Rigidbody_Op, BWU_OT_Physics_Settings_Friction_Op, OBJECT_OT_pyhsics_settings
 from . ObjectContext.context_main import BWU_CONTEXT_MENU_OP
 from . workspace.create_main_workspace import create_editor_workspace_op
-from . polify import make_item_poly
-classes = (BWU_OT_Remove_All_Op, BWU_PHYSICS_PANEL, BWU_OT_Add_Rigidbody_Op, BWU_OT_Add_Rigidbody_Passive_Op, BWU_OT_Remove_Rigidbody_Op,
-           BWU_OT_Physics_Settings_Friction_Op,OBJECT_OT_pyhsics_settings, BWU_CONTEXT_MENU_OP, make_item_poly, POLY_WINDOW_CONTEXT )
+from . polify.make_item_poly import change_lod
+
+classes = (BWU_OT_Apply_All_Op, BWU_OT_Remove_All_Op, BWU_PHYSICS_PANEL, BWU_OT_Add_Rigidbody_Op, BWU_OT_Add_Rigidbody_Passive_Op, BWU_OT_Remove_Rigidbody_Op,
+           BWU_OT_Physics_Settings_Friction_Op,OBJECT_OT_pyhsics_settings, BWU_CONTEXT_MENU_OP, change_lod, POLY_WINDOW_CONTEXT )
 
 def register():
     for c in classes:
@@ -30,5 +31,4 @@ def register():
 def unregister():
     for c in classes:
       bpy.utils.unregister_class(c)
-      bpy.utils.unregister_class(c[0])
 
